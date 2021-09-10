769d521e7ce523cc09a1fdaf329c06c7e16d4e91
DreDreDreDre <andrejar34@gmail.com>
2021-09-10 15:35:26 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,28 @@
+package org.example;
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        double c_to;
+        double c_from;
+        double rate;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("How many euros are you exchanging? ");
+        c_from = sc.nextDouble();
+
+        System.out.print("What is the exchange rate? ");
+        rate = sc.nextDouble();
+
+        c_to = c_from * rate;
+        System.out.printf("%.2f euros at an exchange rate of %f is %.2f U.S. dollars.", c_from, rate, c_to);
+
+        sc.close();
+    }
+}
